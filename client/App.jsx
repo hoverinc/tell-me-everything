ccb2fa660318a8cb3afc225f120b83d44829f437
Thomas Genin <xt6.thomas.genin@gmail.com>
2023-05-26 14:19:51 -0700

    initial frontend

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Root extends React.Component {
+    render() {
+        return <body>
+            <div id="root">
+            </div>
+        </body>;
+    }
+}
+
+
+
+
